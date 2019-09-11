83e0d2251b118214d3a2a369a66d3e8f9dee4d84
Barel Zrian <Barelzrian@me.com>
2019-09-11 10:07:02 +0300

    added some js

--- index.js
+++ index.js
@@ -3,16 +3,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length
 for (var i = 0 ; i < numberOfDrumButtons; i++)
 {
 
-    document.querySelectorAll(".drum")[2].addEventListener("click" , function()
-    {
-    
-     var audio = new Audio('sounds/tom-2.mp3');
-     audio.play();
-  
-        
-    });
-
-    document.querySelectorAll(".drum")[i].addEventListener("click" , function()
+    document.querySelectorAll(".drum")[0].addEventListener("click" , function()
     {
     
      var audio = new Audio('sounds/tom-1.mp3');
@@ -21,11 +12,46 @@ for (var i = 0 ; i < numberOfDrumButtons; i++)
         
     });
 
+    document.querySelectorAll(".drum")[1].addEventListener("click" , function()
+    {
+    
+     var audio = new Audio('sounds/tom-2.mp3');
+     audio.play();
+  
+        
+    });
+
+
+    document.querySelectorAll(".drum")[2].addEventListener("click" , function()
+    {
+    
+     var audio = new Audio('sounds/tom-3.mp3');
+     audio.play();
+  
+        
+    });
 
     document.querySelectorAll(".drum")[3].addEventListener("click" , function()
     {
     
-     var audio = new Audio('sounds/tom-3.mp3');
+     var audio = new Audio('sounds/snare.mp3');
+     audio.play();
+  
+        
+    });
+    document.querySelectorAll(".drum")[4].addEventListener("click" , function()
+    {
+    
+     var audio = new Audio('sounds/kick.mp3');
+     audio.play();
+  
+        
+    });
+
+    document.querySelectorAll(".drum")[5].addEventListener("click" , function()
+    {
+    
+     var audio = new Audio('sounds/crash.mp3');
      audio.play();
   
         
